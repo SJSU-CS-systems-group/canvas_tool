3ac8af0f2a79bf67391dac7abfe491fec3a328b5
bcr33d <bcr33d@gmail.com>
2023-05-18 22:05:57 -0700

    more work on fudge points, still doesn't work

--- canvas_tool.py
+++ canvas_tool.py
@@ -396,8 +396,12 @@ def grade_discussion(course_name, assignment_name, dryrun, min_words, points_com
 @canvas_tool.command()
 @click.argument('course_name')
 @click.argument('quiz_name', default='')
-@click.argument('points', default=-1)
-def set_fudge_points(course_name, quiz_name, points):
+@click.argument('points', default=-666, type=float)
+@click.option('--dryrun/--no-dryrun', default=True, show_default=True,
+        help="only show the grade, don't actually set it")
+@click.option('--decrease/--no-decrease', default=False, show_default=True,
+    help='If not true, the fudge points will not be updated if new points < old points.')
+def set_fudge_points(course_name, quiz_name, points, dryrun, decrease):
     '''
     set the fudge points for a quiz.
 
@@ -422,10 +426,18 @@ def set_fudge_points(course_name, quiz_name, points):
         error(f"multiple matches for {quiz_name}: {', '.join([q.title for q in selected_quizzes])}")
     else:
         for s in selected_quizzes[0].get_submissions():
-            if points == -1:
+            if points == -666:
                 info(f"{s.user_id} {s.fudge_points}")
             else:
-                s.update_score_and_comments(fudge_points=points)
+                if not decrease and s.fudge_points > points:
+                    info(f"skipping {s.user_id} with {s.fudge_points} points")
+                else:
+                    if dryrun:
+                        info(f"would update fudge points for {s.user_id} from {s.fudge_points} to {points}")
+                    else:
+                        info(f"updating fudge points for {s.user_id} from {s.fudge_points} to {points}")
+                        # s.update_score_and_comments(fudge_points=points)
+                        s.update_score_and_comments(quiz_submissions=[{"attempt": s.attempt, 'fudge_points': points}])
 
 
 @canvas_tool.command()
--- make_zipapp.sh
+++ make_zipapp.sh
@@ -2,7 +2,7 @@
 
 rm -rf build
 mkdir build
-PYTHONUSERBASE=$PWD/build python3 -m pip install --ignore-installed click canvasapi
+PYTHONUSERBASE=$PWD/build python3 -m pip install --ignore-installed click canvasapi mosspy
 mkdir build/canvas_tool.app
 mv build/lib/*/site-packages/* build/canvas_tool.app
 cp canvas_tool.py __main__.py build/canvas_tool.app
