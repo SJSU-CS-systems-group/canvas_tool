dc24828a534bba63485c4e44acf71979a1509b69
ben reed <bcr33d@gmail.com>
2023-10-26 12:50:17 -0700

    updated make_zipapp

--- make_zipapp.sh
+++ make_zipapp.sh
@@ -2,8 +2,8 @@
 
 rm -rf build
 mkdir build
-PYTHONUSERBASE=$PWD/build python3 -m pip install --ignore-installed click canvasapi mosspy markdownify
+python3 -m pip install -t $PWD/build/pkgs --ignore-installed click canvasapi mosspy markdownify markdown
 mkdir build/canvas_tool.app
-mv build/lib/*/site-packages/* build/canvas_tool.app
+mv $(find build/pkgs -maxdepth 1 -mindepth 1 -type d)  build/canvas_tool.app
 cp -r commands md2fhtml.py core.py canvas_tool.py __main__.py build/canvas_tool.app
 python3 -m zipapp build/canvas_tool.app
